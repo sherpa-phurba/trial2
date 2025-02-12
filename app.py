4052c8e498ba993c6470016b503d37d1d204cc26
trial <phurbatsheringsherpa333@gmail.com>
2025-02-12 10:37:20 +0545

    Updated requirements.txt

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pickle 
 import pandas as pd
 # To load model back
-with open('SVCpred.pickle','rb') as file:
+with open('News_Classification.pickle','rb') as file:
     loaded_model = pickle.load(file)
 
 st.title("Text Classification")
